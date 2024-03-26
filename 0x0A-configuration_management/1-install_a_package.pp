e57345886df047cdc62a9b86ae3f2eca328588c5
Eseteselase <esete.s.berhanu@gmail.com>
2024-03-26 13:49:44 +0300

    1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# using pupet, install flask
-
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# install puppet-lint
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem',
 }
